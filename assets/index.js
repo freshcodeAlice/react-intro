db441a5fc3c5fa2d64a85d479cea6d78bfa6f751
Alice <freshjs.ofte@gmail.com>
2022-02-18 18:43:29 +0200

    Hello, react!

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,15 @@
 'use strict'
+
+//React
+//ReactDOM
+
+
+
+const reactElement = React.createElement('h1', {
+    title: 'hello', 
+    className: 'heading'
+}, 
+'Hello, React.js!')
+
+
+ReactDOM.render(reactElement, document.getElementById('root'));
