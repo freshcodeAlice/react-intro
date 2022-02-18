ea8e8e83cb3a90ac783a4b7791002791c14d6855
Alice <freshjs.ofte@gmail.com>
2022-02-18 19:34:52 +0200

    Rendering two element at the time

--- assets/index.js
+++ assets/index.js
@@ -3,13 +3,30 @@
 //React
 //ReactDOM
 
+class Heading extends React.Component {
+
+    render(){
+        console.log(this);
+        const {titleForHeading, classForHeading, children} = this.props;
+        
+        return React.createElement('h1',{
+            title: titleForHeading, 
+            className: classForHeading
+        }, 
+        ...children)
+    }
+}
+
+const reactElement = React.createElement(Heading, {
+    titleForHeading: 'hi!',
+    classForHeading: 'heading'
+}, 'str1'); 
+
+const reactElement2 = React.createElement(Heading, {
+    classForHeading: 'new-heading'
+}, 'New Heading');
+
+const reactWrapperElement = React.createElement('div', null, reactElement, reactElement2);
 
 
-const reactElement = React.createElement('h1', {
-    title: 'hello', 
-    className: 'heading'
-}, 
-'Hello, React.js!')
-
-
-ReactDOM.render(reactElement, document.getElementById('root'));
+ReactDOM.render(reactWrapperElement, document.getElementById('root'));
