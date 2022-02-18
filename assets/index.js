ea18f8c3b3f2ad2fc0ae6f6d36e614f452bbdebb
Alice <freshjs.ofte@gmail.com>
2022-02-18 20:31:57 +0200

    Non-zero task resolve

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,5 @@
 'use strict'
 
-//React
-//ReactDOM
-
 class Counter extends React.Component {
     constructor(props){
         super(props);
@@ -17,14 +14,16 @@ class Counter extends React.Component {
        })
     }
     decrement = () => {
-        this.setState({
-            counter: this.state.counter - 1
-        })
+        if(this.state.counter > 0 ) {
+            this.setState({
+                counter: this.state.counter - 1
+            })
+        }
     }
 
     render () {
         const {counter } = this.state;
-        console.log(counter);
+        console.log(this);
         return React.createElement(React.Fragment, null, 
             React.createElement('h1', null, counter), 
             React.createElement('button',{
